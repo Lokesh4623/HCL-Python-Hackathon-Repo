c19a52a8cf9dcb6047b0fb74ac59ee1d162de355
lokesh4623 <lokeshthirupathi08@gmail.com>
2025-10-26 15:00:42 +0530

    working code committed

--- schemas.py
+++ schemas.py
@@ -4,12 +4,6 @@ class AccountRequest(BaseModel):
     account_type: str
     initial_deposit: float
 
-    # @validator("account_type")
-    # def validate_account_type(cls, v):
-    #     if v not in ["Savings", "Current", "FD"]:
-    #         return {"message":"Account type must be Savings, Current, or FD"}
-    #     return v
-
 class TokenRequest(BaseModel):
     pan_number: str
 
